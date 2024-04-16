4bd6dc0b41ab0c968b95ec70fe0be071c0399384
jaloliddin99 <jaloliddin49@gmail.com>
2024-04-16 17:38:01 +0500

    update note is implemented

--- src/main/java/uz/cau/notetakingapplication/Notes.java
+++ src/main/java/uz/cau/notetakingapplication/Notes.java
@@ -8,7 +8,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
